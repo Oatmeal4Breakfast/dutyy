684f400ff1718e05b07f1f7c55408ddc3c0ba348
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-03-29 22:36:38 -0400

    add: assets for README.md

--- src/db.py
+++ src/db.py
@@ -10,18 +10,8 @@ from platformdirs import user_data_dir
 
 from src.models.schemas import Base
 
-# PROJECT_ROOT: Path = Path(__file__).resolve().parent.parent
 
 APP_NAME: str = "dutyy"
-
-# def _build_db_uri(config: Config) -> str:
-#     if config.db_uri.startswith("sqlite:///"):
-#         db_file_path: str = config.db_uri.replace("sqlite:///", "")
-#         db_abs_path: Path = PROJECT_ROOT / db_file_path
-#         db_abs_path.parent.mkdir(parents=True, exist_ok=True)
-#         return f"sqlite+aiosqlite:///{db_abs_path}"
-#     else:
-#         raise ValueError(f"{config.db_uri} is not a valid sqlite uri")
 
 
 def _get_db_uri() -> str:
@@ -36,8 +26,6 @@ def _create_db_engine(db_uri: str) -> AsyncEngine:
     return create_async_engine(url=db_uri, connect_args=connect_args)
 
 
-# config: Config = get_config()
-# db_uri: str = _build_db_uri(config=config)
 db_uri: str = _get_db_uri()
 engine: AsyncEngine = _create_db_engine(db_uri=db_uri)
 SessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(bind=engine)
