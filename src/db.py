43d877239111e7981fbf8f2f2321b7f216df229f
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-03-29 22:19:06 -0400

    update: move from config base db_uri to xdg_home using platformdirs

--- src/db.py
+++ src/db.py
@@ -6,21 +6,29 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     AsyncEngine,
 )
+from platformdirs import user_data_dir
 
 from src.models.schemas import Base
-from src.config import Config, get_config
 
-PROJECT_ROOT: Path = Path(__file__).resolve().parent.parent
+# PROJECT_ROOT: Path = Path(__file__).resolve().parent.parent
+
+APP_NAME: str = "dutyy"
+
+# def _build_db_uri(config: Config) -> str:
+#     if config.db_uri.startswith("sqlite:///"):
+#         db_file_path: str = config.db_uri.replace("sqlite:///", "")
+#         db_abs_path: Path = PROJECT_ROOT / db_file_path
+#         db_abs_path.parent.mkdir(parents=True, exist_ok=True)
+#         return f"sqlite+aiosqlite:///{db_abs_path}"
+#     else:
+#         raise ValueError(f"{config.db_uri} is not a valid sqlite uri")
 
 
-def _build_db_uri(config: Config) -> str:
-    if config.db_uri.startswith("sqlite:///"):
-        db_file_path: str = config.db_uri.replace("sqlite:///", "")
-        db_abs_path: Path = PROJECT_ROOT / db_file_path
-        db_abs_path.parent.mkdir(parents=True, exist_ok=True)
-        return f"sqlite+aiosqlite:///{db_abs_path}"
-    else:
-        raise ValueError(f"{config.db_uri} is not a valid sqlite uri")
+def _get_db_uri() -> str:
+    data_dir: Path = Path(user_data_dir(appname=APP_NAME))
+    data_dir.mkdir(parents=True, exist_ok=True)
+    path: Path = data_dir / "database.db"
+    return f"sqlite+aiosqlite:///{path}"
 
 
 def _create_db_engine(db_uri: str) -> AsyncEngine:
@@ -28,8 +36,9 @@ def _create_db_engine(db_uri: str) -> AsyncEngine:
     return create_async_engine(url=db_uri, connect_args=connect_args)
 
 
-config: Config = get_config()
-db_uri: str = _build_db_uri(config=config)
+# config: Config = get_config()
+# db_uri: str = _build_db_uri(config=config)
+db_uri: str = _get_db_uri()
 engine: AsyncEngine = _create_db_engine(db_uri=db_uri)
 SessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(bind=engine)
 
